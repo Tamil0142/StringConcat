73ee0a364ab17b0deb61bfa17212f73859917218
Tamil0142 <42166616+Tamil0142@users.noreply.github.com>
2018-08-22 10:58:56 +0530

    Create StringConcat.java

---
+++ StringConcat.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class wifi{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		String a=in.next();
+		String b=in.next();
+		System.out.println(a+b);
+		
+}}
